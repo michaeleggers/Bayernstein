c1207e26219985512c3db56006f9f56cac728476
benek <benedikt_koehler@hotmail.com>
2024-10-12 16:57:31 +0200

    add basic ci

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,1 +1,1 @@
-find . -path ./dependencies -prune -o \( -name '*.cpp' -o -name '*.h' \) -print | xargs clang-tidy -p build/
+find . \( -path ./dependencies -o -path ./build \) -prune -o \( -name '*.cpp' -o -name '*.h' \) -print | xargs clang-tidy -p build/
