206145f866f127e2c988c332d87ab92cf417b680
Michael Eggers <mseggers@protonmail.ch>
2024-11-12 18:59:36 +0100

    fix: Add some comments to the fly-cam-entity

--- Entity/FlyCamera/g_fly_camera.h
+++ Entity/FlyCamera/g_fly_camera.h
@@ -13,6 +13,16 @@
 #include "../../dependencies/glm/ext.hpp"
 #include "../../dependencies/glm/glm.hpp"
 
+/* 
+* This entity encapsulates a regular Camera. It implements
+* the InputReceiver to get high-level input commands.
+* You can fly around in the world with this camera by
+* holding the right mouse-button and using WASD to 
+* move forwars/backwars/left/righ.
+* TODO: We could think about having the camera itself being a
+* BaseGameEntity. But I am not sure about this. We have to
+* think about Entities a bit more anyways later.
+*/
 class CFlyCamera : public BaseGameEntity, public IInputReceiver {
 
 public:
